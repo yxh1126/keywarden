61656879085e7d12ec0e704538d1cffaf7dd42c2
yi.huang <yi.huang@inceptioglobal.ai>
2024-01-02 18:17:43 -0800

    update fmt

--- version/version_hdr.h
+++ version/version_hdr.h
@@ -1,10 +1,10 @@
 //
-// Copyright 2022 Inceptio Technology. All Rights Reserved.
+// Copyright 2023 Yi Huang. All Rights Reserved.
 //
 
 #ifndef VERSION_VERSION_HDR_H_
 #define VERSION_VERSION_HDR_H_
 
-#define CRYPTO_TOOL_VERSION_NUM "SRC_VER_INFO"
+#define KEYWARDEN_VERSION_NUM "SRC_VER_INFO"
 
 #endif  // VERSION_VERSION_HDR_H_
