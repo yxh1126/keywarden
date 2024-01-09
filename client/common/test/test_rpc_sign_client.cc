df8f15818c7641b0301f2f1cf8cc65d05bd487d3
yi.huang <yi.huang@inceptioglobal.ai>
2024-01-08 17:21:18 -0800

    test case opt

--- client/common/test/test_rpc_sign_client.cc
+++ client/common/test/test_rpc_sign_client.cc
@@ -67,11 +67,14 @@ TEST_F(RpcSignClientTest, TestVerifyPubkeyHash) {
   int key_set[] = {RSA_1024_KEY_SET, RSA_2048_KEY_SET};
   int pub_type = JOB_J5_PUB_DER;
   std::string pub_key_fpt;
-  std::vector<std::string> hash_list;
+  std::vector<std::string> hash_list, hash_list_ex;
   int idx = 0;
 
   EXPECT_TRUE(FmtUtils::ReadText("data/hashlist.txt", &hash_list));
+  EXPECT_TRUE(FmtUtils::ReadText("data/hashlist_ex.txt", &hash_list_ex));
+
   EXPECT_EQ(hash_list.size(), SUPRT_KEY_SET * SUPRT_KEY_ID);
+  EXPECT_EQ(hash_list_ex.size(), SUPRT_KEY_SET * SUPRT_KEY_ID);
 
   for (int set = 0; set < SUPRT_KEY_SET; set++) {
     for (int id = 0; id < SUPRT_KEY_ID; id++) {
@@ -81,29 +84,9 @@ TEST_F(RpcSignClientTest, TestVerifyPubkeyHash) {
 
       pub_key_fpt = CryptoUtils::GetRsaPubKeyHash(pub_key_der, key_set[set]);
       EXPECT_EQ(pub_key_fpt, hash_list[idx]);
-      idx++;
-    }
-  }
-}
-
-TEST_F(RpcSignClientTest, TestVerifyPubkeyHashPem) {
-  int key_set[] = {RSA_1024_KEY_SET, RSA_2048_KEY_SET};
-  int pub_type = JOB_J5_PUB_DER;
-  std::string pub_key_fpt;
-  std::vector<std::string> hash_list;
-  int idx = 0;
-
-  EXPECT_TRUE(FmtUtils::ReadText("data/hashlist_ex.txt", &hash_list));
-  EXPECT_EQ(hash_list.size(), SUPRT_KEY_SET * SUPRT_KEY_ID);
-
-  for (int set = 0; set < SUPRT_KEY_SET; set++) {
-    for (int id = 0; id < SUPRT_KEY_ID; id++) {
-      std::string pub_key_der =
-        client->GetRsaPublicKey(key_set[set], id + 1, pub_type);
-      if (pub_key_der == RPC_FAILURE_MSG) break;
 
       pub_key_fpt = CryptoUtils::GetRsaPubKeyHash(pub_key_der, -1);
-      EXPECT_EQ(pub_key_fpt, hash_list[idx]);
+      EXPECT_EQ(pub_key_fpt, hash_list_ex[idx]);
       idx++;
     }
   }
